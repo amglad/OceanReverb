771f02f7b026e641a1f9c36b81ced8dc0dfe7243
amglad <amglad@icloud.com>
2023-05-15 16:16:09 -0500

    remove all wav files

--- OceanVerb/JuceLibraryCode/BinaryData.h
+++ OceanVerb/JuceLibraryCode/BinaryData.h
@@ -8,14 +8,11 @@
 
 namespace BinaryData
 {
-    extern const char*   Side_angle_12_feet_up96k_wav;
-    const int            Side_angle_12_feet_up96k_wavSize = 1333036;
-
     extern const char*   waves_png;
     const int            waves_pngSize = 47581;
 
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 2;
+    const int namedResourceListSize = 1;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
